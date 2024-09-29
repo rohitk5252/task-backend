746ae5064b435fa14ff09f02794e395b137794a0
Rohit Kumar <rohitofficial2001@gmail.com>
2024-09-29 14:45:08 +0530

    Bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,7 +44,7 @@ const googleAuth = async (req, res) => {
     try {
         const { tokens } = await client.getToken({
             code,
-            redirect_uri: "https://task-backend-bliy.onrender.com"
+            redirect_uri: "https://task-frontend-bwbo.onrender.com"
         });
         client.setCredentials(tokens);
         const ticket = await client.verifyIdToken({
